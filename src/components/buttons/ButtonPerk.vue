df9ce2fd3d1784020677af37c0cf363fb4e7528b
joey tierney <joey.tierney@marinosoftware.com>
2022-08-08 11:48:28 +0100

    feat: perk button component created

---
+++ src/components/buttons/ButtonPerk.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    src="https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/general-icons/perk-button.png"
+    alt="Click here for a random set of Perks!"
+    @click="onClick"
+    class="perkButton"
+  />
+</template>
+
+<style scoped>
+.perkButton {
+  cursor: pointer;
+  width: 9.375rem;
+  height: 9.375rem;
+}
+
+.perkButton:active {
+  transform: translateY(0.25rem);
+}
+</style>
