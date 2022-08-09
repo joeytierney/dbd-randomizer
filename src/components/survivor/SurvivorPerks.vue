e9d8fbcdaa30e961ab54f370ba0f5ee024d8404c
joey tierney <joey.tierney@marinosoftware.com>
2022-08-09 10:28:30 +0100

    feat: basic layout of random perks added

---
+++ src/components/survivor/SurvivorPerks.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Survivor Perks</h1>
+  <div class="perks">
+    <div class="first-perk"></div>
+    <div class="second-perk"></div>
+    <div class="third-perk"></div>
+    <div class="fourth-perk"></div>
+  </div>
+</template>
