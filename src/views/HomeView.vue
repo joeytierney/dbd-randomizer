8296f922bb24e79f71a06a3a99c56dcc83406e94
joey tierney <joey.tierney@marinosoftware.com>
2022-08-09 10:29:15 +0100

    chore: removed HelloWorld component from HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,9 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
