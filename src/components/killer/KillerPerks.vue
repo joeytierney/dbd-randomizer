22aa91ae2ed12305db91840521c0292774d8caa1
joey tierney <joey.tierney@marinosoftware.com>
2022-08-10 09:26:20 +0100

    feat: reroll button imported

--- src/components/killer/KillerPerks.vue
+++ src/components/killer/KillerPerks.vue
@@ -5,24 +5,38 @@
       <img :src="perkOne.image" :alt="perkOne.name" />
       <h1>{{ perkOne.name }}</h1>
       <h3>{{ perkOne.teachable }}</h3>
+      <ButtonReRoll @click="rerollOne" />
     </div>
     <div class="selected-perk">
       <img :src="perkTwo.image" :alt="perkTwo.name" />
       <h1>{{ perkTwo.name }}</h1>
       <h3>{{ perkTwo.teachable }}</h3>
+      <ButtonReRoll @click="rerollTwo" />
     </div>
     <div class="selected-perk">
       <img :src="perkThree.image" :alt="perkThree.name" />
       <h1>{{ perkThree.name }}</h1>
       <h3>{{ perkThree.teachable }}</h3>
+      <ButtonReRoll @click="rerollThree" />
     </div>
     <div class="selected-perk">
       <img :src="perkFour.image" :alt="perkFour.name" />
       <h1>{{ perkFour.name }}</h1>
       <h3>{{ perkFour.teachable }}</h3>
+      <ButtonReRoll @click="rerollFour" />
     </div>
   </div>
 </template>
+
+<script>
+import ButtonReRoll from "@/components/buttons/ButtonReRoll.vue";
+
+export default {
+  components: {
+    ButtonReRoll,
+  },
+}
+</script>
 
 <style scoped>
 @media screen and (max-width: 600px) {
