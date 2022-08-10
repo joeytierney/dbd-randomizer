479e9a4de6a83c135780c00908f4ebd51a86ba5d
joey tierney <joey.tierney@marinosoftware.com>
2022-08-10 09:24:16 +0100

    feat: randomized killer perk skeleton

---
+++ src/components/killer/KillerPerks.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Killer Perks</h1>
+  <div v-if="visible" class="perks">
+    <div class="selected-perk">
+      <img :src="perkOne.image" :alt="perkOne.name" />
+      <h1>{{ perkOne.name }}</h1>
+      <h3>{{ perkOne.teachable }}</h3>
+    </div>
+    <div class="selected-perk">
+      <img :src="perkTwo.image" :alt="perkTwo.name" />
+      <h1>{{ perkTwo.name }}</h1>
+      <h3>{{ perkTwo.teachable }}</h3>
+    </div>
+    <div class="selected-perk">
+      <img :src="perkThree.image" :alt="perkThree.name" />
+      <h1>{{ perkThree.name }}</h1>
+      <h3>{{ perkThree.teachable }}</h3>
+    </div>
+    <div class="selected-perk">
+      <img :src="perkFour.image" :alt="perkFour.name" />
+      <h1>{{ perkFour.name }}</h1>
+      <h3>{{ perkFour.teachable }}</h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 600px) {
+  .selected-perk {
+    width: 100%;
+  }
+}
+
+.selected-perk {
+  float: left;
+  width: 25%;
+}
+
+.perks:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
