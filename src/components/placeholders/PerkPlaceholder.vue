f0a97a6dff3568d1a32664495c05c30e04db690b
joey tierney <joey.tierney@marinosoftware.com>
2022-08-10 13:11:39 +0100

    feat: PerkPlaceholder component

---
+++ src/components/placeholders/PerkPlaceholder.vue
@@ -0,0 +1,7 @@
+<template>
+  <img
+    src="https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/general-icons/random-perk-icon.png"
+    alt="Placholder Perk Icon"
+  />
+  <p>Click the icon below!</p>
+</template>
