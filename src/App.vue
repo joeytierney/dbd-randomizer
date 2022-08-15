0e311d1939d182f7a95acc766442eebdd91491e0
joey tierney <joey.tierney@marinosoftware.com>
2022-08-15 10:46:04 +0100

    chore: background image and main header styles

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,59 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  background: url(https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/background-image/web-background.jpg)
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 3rem;
+  margin: 0 0 1rem 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+}
+
+h2 {
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 1.5rem;
+  margin: 0 0 1rem 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #aa0101;
+}
+
+h3 {
+  font-size: 1rem;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 1rem;
+  margin: 0 0 1rem 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #cccccc;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
