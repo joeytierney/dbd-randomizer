03dee0578e58293e7e9350af50f034412ce36638
joey tierney <joey.tierney@marinosoftware.com>
2022-08-22 10:15:04 +0100

    feat: router-links to navbar

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -4,6 +4,16 @@
       <a class="navbar-brand" href="#">
         <img src="https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/general-icons/dbd-mini-logo.png" alt="Dead by Daylight logo" width="40" height="40">
       </a>
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav mr-auto">
+          <router-link to="/" class="nav-item nav-link">Home</router-link>
+          <router-link to="/about" class="nav-item nav-link">About</router-link>
+          <router-link to="/survivor" class="nav-item nav-link">Random Survivor</router-link>
+          <router-link to="/killer" class="nav-item nav-link">Random Killer</router-link>
+          <router-link to="/survivor-perks" class="nav-item nav-link">Survivor Perks</router-link>
+          <router-link to="/killer-perks" class="nav-item nav-link">Killer Perks</router-link>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
