e18592b543dddb89bf5e2d10c608165fdede58cc
joey tierney <joey.tierney@marinosoftware.com>
2022-08-22 10:10:23 +0100

    feat: NavBar component created, DBD logo added and transparent design

---
+++ src/components/navbar/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <nav class="container navbar navbar-expand-lg backdrop-filter backdrop-blur-lg">
+      <a class="navbar-brand" href="#">
+        <img src="https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/general-icons/dbd-mini-logo.png" alt="Dead by Daylight logo" width="40" height="40">
+      </a>
+    </nav>
+  </div>
+</template>
