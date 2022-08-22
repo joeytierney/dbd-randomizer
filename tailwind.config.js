cffd1c2d111c3b3844354b0c298227e430676456
joey tierney <joey.tierney@marinosoftware.com>
2022-08-22 10:58:30 +0100

    feat: tailwind config file to include firefox navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
@@ -8,18 +8,18 @@ module.exports = {
   },
   plugins: [
     plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-      const isFirefoxRule = postcss.atRule({
-        name: '-moz-document',
-        params: 'url-prefix()',
-      });
-      isFirefoxRule.append(container.nodes);
-      container.append(isFirefoxRule);
-      isFirefoxRule.walkRules((rule) => {
-        rule.selector = `.${e(
-        `firefox${separator}${rule.selector.slice(1)}`
-        )}`;
-      });
+      addVariant("firefox", ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: "-moz-document",
+          params: "url-prefix()",
+        });
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`;
+        });
       });
     }),
   ],
