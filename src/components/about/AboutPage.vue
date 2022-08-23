c564db61ae98e7b40c27ce2ba06a37420a2f0807
joey tierney <joey.tierney@marinosoftware.com>
2022-08-23 09:49:14 +0100

    feat: AboutPage component and content created

---
+++ src/components/about/AboutPage.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="about">
+    <h2>About</h2>
+    <h3>This website</h3>
+    <p>
+      <span>
+        Tired of playing as the same character? Do you already have all perks
+        unlocked? Do you need a new challenge? Want to try some perks you never
+        used before?
+      </span>
+      <span>
+        <b>Then is this the website you searched for.</b>
+      </span>
+    </p>
+    <br />
+    <p>
+      <span>
+        This site randomly selects your Killer, Survivor, and four Killer or
+        Survivor Perks for you to play with in a game of Dead by Daylight.
+      </span>
+    </p>
+    <br />
+    <h3>The game</h3>
+    <p>
+      <span>
+        <a
+          class="link"
+          href="https://deadbydaylight.com"
+          target="_blank"
+          title="https://deadbydaylight.com"
+          rel="noreferrer"
+        >
+          Dead by Daylight
+        </a>
+        is an asymmetrical multiplayer (4vs1) horror game where one player takes
+        on the role of the savage Killer, and the other four players play as
+        Survivors, trying to escape the Killer and avoid being caught and
+        killed.
+      </span>
+    </p>
+    <br />
+    <div>
+      <h2>Disclaimer</h2>
+      <p>
+        <span>
+          <b
+            >This site is not associated with, endorsed by, or in any other form
+            connected with any company.</b
+          >
+        </span>
+      </p>
+      <br />
+      <p>
+        <span>
+          DEAD BY DAYLIGHT® is a registered trademark of Behaviour Interactive
+          Inc. in Canada, the United States of America and/or other countries.
+        </span>
+      </p>
+      <p>
+        <span>
+          Use at your own risk. This site runs in your browser, no data is saved
+          on the server.
+        </span>
+      </p>
+      <br />
+      <h3>Image credit</h3>
+      <p>
+        <span>
+          The character portraits, perk icons and other in-game icons are game
+          related content and therefore property of
+          <a
+            class="link"
+            href="https://www.bhvr.com/"
+            target="_blank"
+            title="https://www.bhvr.com/"
+            rel="noreferrer"
+            >Behaviour
+          </a>
+          and were taken from the official
+          <a
+            class="link"
+            href="https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki"
+            target="_blank"
+            title="https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki"
+            rel="noreferrer"
+            >Dead by Daylight wiki </a
+          >.
+        </span>
+      </p>
+      <br />
+      <p>
+        <span>All other images were created by the author.</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  color: #fff;
+}
+
+h2 {
+  font-size: 2.4rem;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
