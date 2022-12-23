b0bb43d29c2c0b796d05412479a686ee4da7b8a5
Joey Tierney <joeytierney_@hotmail.com>
2022-12-23 21:29:24 +0000

    adding dist subtree

--- src/components/placeholders/PerkPlaceholder.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <img
-    src="https://raw.githubusercontent.com/joeytierney/dbd-vue/develop/src/assets/general-icons/random-perk-icon.png"
-    alt="Placholder Perk Icon"
-  />
-</template>
